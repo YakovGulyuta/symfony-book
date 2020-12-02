82df0192ca39d367176eec63a53e4be64afa4654
Yakov <yakovgulyuta@yandex.ru>
2020-12-02 15:44:29 +0300

    добавили глобально переменную во все шаблоны

--- src/EventSubscriber/TwigEventSubscriber.php
+++ src/EventSubscriber/TwigEventSubscriber.php
@@ -2,20 +2,32 @@
 
 namespace App\EventSubscriber;
 
+use App\Repository\ConferenceRepository;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Event\ControllerEvent;
+use Twig\Environment;
 
 class TwigEventSubscriber implements EventSubscriberInterface
 {
-    public function onKernelController(ControllerEvent $event)
-    {
-        // ...
-    }
 
-    public static function getSubscribedEvents()
-    {
-        return [
-            'kernel.controller' => 'onKernelController',
-        ];
-    }
+  private $twig;
+  private $conferenceRepository;
+
+  public function __construct(Environment $twig, ConferenceRepository $conferenceRepository)
+  {
+    $this->twig = $twig;
+    $this->conferenceRepository = $conferenceRepository;
+  }
+
+  public function onKernelController(ControllerEvent $event)
+  {
+    $this->twig->addGlobal('conferences', $this->conferenceRepository->findAll());
+  }
+
+  public static function getSubscribedEvents()
+  {
+    return [
+      'kernel.controller' => 'onKernelController',
+    ];
+  }
 }
