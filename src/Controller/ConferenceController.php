db32446f23054b1c7c9bb8ac9f167d4e593a3873
Yakov <yakovgulyuta@yandex.ru>
2020-12-02 13:43:24 +0300

    удобавил Twig для главной и просмотры комментариев + пагинация

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -2,19 +2,65 @@
 
 namespace App\Controller;
 
+use App\Entity\Conference;
+use App\Repository\CommentRepository;
+use App\Repository\ConferenceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
 class ConferenceController extends AbstractController
 {
-    /**
-     * @Route("/", name="homepage")
-     */
-    public function index(): Response
-    {
-        return $this->render('conference/index.html.twig', [
-            'controller_name' => 'ConferenceController',
-        ]);
-    }
+  private $twig;
+
+  public function __construct(Environment $twig)
+  {
+    $this->twig = $twig;
+  }
+
+  /**
+   * @Route("/", name="homepage")
+   */
+  public function index(ConferenceRepository $conferenceRepository): Response
+  {
+
+    return new Response($this->twig->render('conference/index.html.twig', [
+      'conferences' => $conferenceRepository->findAll()
+    ]));
+
+//        return $this->render('conference/index.html.twig', [
+//            'controller_name' => 'ConferenceController',
+//        ]);
+  }
+
+  /**
+   * @Route("/conference/{id}", name="conference")
+   */
+
+  public function show(Request $request, Conference $conference, CommentRepository $commentRepository)
+  {
+
+    $offset = max(0, $request->query->getInt('offset', 0));
+    $paginator = $commentRepository->getCommentPaginator($conference, $offset);
+
+    return new Response($this->twig->render('conference/show.html.twig', [
+      'conference' => $conference,
+      'comments' => $paginator,
+      'previous' => $offset - CommentRepository::PAGINATOR_PER_PAGE,
+      'next' => min(count($paginator), $offset + CommentRepository::PAGINATOR_PER_PAGE),
+    ]));
+
+  }
+
+//  public function show(Environment $twig, Conference $conference, CommentRepository $commentRepository)
+//  {
+//
+//    return new Response($twig->render('conference/show.html.twig', [
+//        'conference' => $conference,
+//        'comments' => $commentRepository->findBy(['conference' => $conference], ['createdAt'=>'DESC'])
+//    ]));
+//
+//  }
 }
