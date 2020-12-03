e8bf0d936fbf48563ff59597128913f3f16edb5a
Yakov <yakovgulyuta@yandex.ru>
2020-12-03 15:40:44 +0300

    composer req workflow

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Comment;
 use App\Entity\Conference;
 use App\Form\CommentFormType;
+use App\Message\CommentMessage;
 use App\Repository\CommentRepository;
 use App\Repository\ConferenceRepository;
 use Doctrine\ORM\EntityManagerInterface;
@@ -12,6 +13,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Twig\Environment;
 
@@ -19,43 +21,37 @@ class ConferenceController extends AbstractController
 {
   private $twig;
   private $entityManager;
+  private $bus;
 
-  public function __construct(Environment $twig, EntityManagerInterface $entityManager)
+  public function __construct(Environment $twig, EntityManagerInterface $entityManager, MessageBusInterface $bus)
   {
     $this->twig = $twig;
     $this->entityManager = $entityManager;
+    $this->bus = $bus;
   }
 
   /**
    * @Route("/", name="homepage")
    */
-  public function index(ConferenceRepository $conferenceRepository): Response
+  public function index(ConferenceRepository $conferenceRepository)
   {
-
     return new Response($this->twig->render('conference/index.html.twig', [
-      'conferences' => $conferenceRepository->findAll()
+      'conferences' => $conferenceRepository->findAll(),
     ]));
-
-//        return $this->render('conference/index.html.twig', [
-//            'controller_name' => 'ConferenceController',
-//        ]);
   }
 
   /**
    * @Route("/conference/{slug}", name="conference")
    */
-
   public function show(Request $request, Conference $conference, CommentRepository $commentRepository, string $photoDir)
   {
-
     $comment = new Comment();
     $form = $this->createForm(CommentFormType::class, $comment);
-
     $form->handleRequest($request);
     if ($form->isSubmitted() && $form->isValid()) {
       $comment->setConference($conference);
       if ($photo = $form['photo']->getData()) {
-        $filename = bin2hex(random_bytes(6)) . '.' . $photo->guessExtension();
+        $filename = bin2hex(random_bytes(6)).'.'.$photo->guessExtension();
         try {
           $photo->move($photoDir, $filename);
         } catch (FileException $e) {
@@ -66,6 +62,15 @@ class ConferenceController extends AbstractController
 
       $this->entityManager->persist($comment);
       $this->entityManager->flush();
+
+      $context = [
+        'user_ip' => $request->getClientIp(),
+        'user_agent' => $request->headers->get('user-agent'),
+        'referrer' => $request->headers->get('referer'),
+        'permalink' => $request->getUri(),
+      ];
+
+      $this->bus->dispatch(new CommentMessage($comment->getId(), $context));
 
       return $this->redirectToRoute('conference', ['slug' => $conference->getSlug()]);
     }
@@ -80,16 +85,5 @@ class ConferenceController extends AbstractController
       'next' => min(count($paginator), $offset + CommentRepository::PAGINATOR_PER_PAGE),
       'comment_form' => $form->createView(),
     ]));
-
   }
-
-//  public function show(Environment $twig, Conference $conference, CommentRepository $commentRepository)
-//  {
-//
-//    return new Response($twig->render('conference/show.html.twig', [
-//        'conference' => $conference,
-//        'comments' => $commentRepository->findBy(['conference' => $conference], ['createdAt'=>'DESC'])
-//    ]));
-//
-//  }
 }
