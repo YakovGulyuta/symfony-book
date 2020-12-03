7d4f26f88774aaee046264c54981a8961fd2e6b8
Yakov <yakovgulyuta@yandex.ru>
2020-12-03 12:35:52 +0300

    сделали и накатили миграцию Админ

--- src/Entity/Admin.php
+++ src/Entity/Admin.php
@@ -11,99 +11,104 @@ use Symfony\Component\Security\Core\User\UserInterface;
  */
 class Admin implements UserInterface
 {
-    /**
-     * @ORM\Id
-     * @ORM\GeneratedValue
-     * @ORM\Column(type="integer")
-     */
-    private $id;
+  /**
+   * @ORM\Id
+   * @ORM\GeneratedValue
+   * @ORM\Column(type="integer")
+   */
+  private $id;
 
-    /**
-     * @ORM\Column(type="string", length=180, unique=true)
-     */
-    private $username;
+  /**
+   * @ORM\Column(type="string", length=180, unique=true)
+   */
+  private $username;
 
-    /**
-     * @ORM\Column(type="json")
-     */
-    private $roles = [];
+  /**
+   * @ORM\Column(type="json")
+   */
+  private $roles = [];
 
-    /**
-     * @var string The hashed password
-     * @ORM\Column(type="string")
-     */
-    private $password;
+  /**
+   * @var string The hashed password
+   * @ORM\Column(type="string")
+   */
+  private $password;
 
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
+  public function getId(): ?int
+  {
+    return $this->id;
+  }
 
-    /**
-     * A visual identifier that represents this user.
-     *
-     * @see UserInterface
-     */
-    public function getUsername(): string
-    {
-        return (string) $this->username;
-    }
+  /**
+   * A visual identifier that represents this user.
+   *
+   * @see UserInterface
+   */
+  public function getUsername(): string
+  {
+    return (string)$this->username;
+  }
 
-    public function setUsername(string $username): self
-    {
-        $this->username = $username;
+  public function setUsername(string $username): self
+  {
+    $this->username = $username;
 
-        return $this;
-    }
+    return $this;
+  }
 
-    /**
-     * @see UserInterface
-     */
-    public function getRoles(): array
-    {
-        $roles = $this->roles;
-        // guarantee every user at least has ROLE_USER
-        $roles[] = 'ROLE_USER';
+  /**
+   * @see UserInterface
+   */
+  public function getRoles(): array
+  {
+    $roles = $this->roles;
+    // guarantee every user at least has ROLE_USER
+    $roles[] = 'ROLE_USER';
 
-        return array_unique($roles);
-    }
+    return array_unique($roles);
+  }
 
-    public function setRoles(array $roles): self
-    {
-        $this->roles = $roles;
+  public function setRoles(array $roles): self
+  {
+    $this->roles = $roles;
 
-        return $this;
-    }
+    return $this;
+  }
 
-    /**
-     * @see UserInterface
-     */
-    public function getPassword(): string
-    {
-        return (string) $this->password;
-    }
+  /**
+   * @see UserInterface
+   */
+  public function getPassword(): string
+  {
+    return (string)$this->password;
+  }
 
-    public function setPassword(string $password): self
-    {
-        $this->password = $password;
+  public function setPassword(string $password): self
+  {
+    $this->password = $password;
 
-        return $this;
-    }
+    return $this;
+  }
 
-    /**
-     * @see UserInterface
-     */
-    public function getSalt()
-    {
-        // not needed when using the "bcrypt" algorithm in security.yaml
-    }
+  /**
+   * @see UserInterface
+   */
+  public function getSalt()
+  {
+    // not needed when using the "bcrypt" algorithm in security.yaml
+  }
 
-    /**
-     * @see UserInterface
-     */
-    public function eraseCredentials()
-    {
-        // If you store any temporary, sensitive data on the user, clear it here
-        // $this->plainPassword = null;
-    }
+  /**
+   * @see UserInterface
+   */
+  public function eraseCredentials()
+  {
+    // If you store any temporary, sensitive data on the user, clear it here
+    // $this->plainPassword = null;
+  }
+
+  public function __toString(): string
+  {
+    return $this->username;
+  }
 }
